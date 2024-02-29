994fbdcf0d5f27f606922208455c0027148977ff
taruniyadav <149263546+taruniyadav@users.noreply.github.com>
2024-02-29 12:52:55 +0530

    Add files via upload
    basic java programs

---
+++ Add.java
@@ -0,0 +1,11 @@
+
+class Add{
+     public static void main(String[]args){
+       Scanner sc=new Scanner(System.in);
+       System.out.println("enter the values of a and b");
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       System.out.println("addition of "+a+" + "+b+" is" + (a+b) );
+       }
+   }
+       
