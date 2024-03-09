0c39568f9f0f39965525cd0a33b9d158786fa774
taruniyadav <149263546+taruniyadav@users.noreply.github.com>
2024-03-09 15:25:17 +0530

    Add files via upload
    1-100 numbers printing

---
+++ num.java
@@ -0,0 +1,12 @@
+class num{
+ public static void main(String[]mini){
+ int a=1;
+    for(int i=1;i<=10;i++){
+     for(int j=1;j<=10;j++){
+   System.out.print(" "+a);
+   a++;
+    }
+   System.out.println("");
+   } 
+  }
+}
